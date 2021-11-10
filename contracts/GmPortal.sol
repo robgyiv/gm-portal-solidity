a5af5bea785c724324597a9ed8c631a0865e9d11
Robbie R <git@robbie.computer>
2021-11-10 22:49:00 +0000

    Choose 1/2 winners, throw error if sent within 15 minutes

--- contracts/GmPortal.sol
+++ contracts/GmPortal.sol
@@ -6,6 +6,7 @@ import "hardhat/console.sol";
 
 contract GmPortal {
     uint256 totalGms;
+    uint256 seed;
 
     // Solidity event
     event NewGm(address indexed from, uint256 timestamp, string message);
@@ -18,19 +19,31 @@ contract GmPortal {
 
     Gm[] gms;
 
+    mapping(address => uint256) public lastGmedAt;
+
     constructor() payable {
         console.log("Hello, I'm a contract and I'm smart");
+
+        seed = (block.timestamp + block.difficulty) % 100;
     }
 
     function gm(string memory _message) public {
+        require(
+            lastGmedAt[msg.sender] + 15 minutes < block.timestamp,
+            "Wait 15m"
+        );
+
+        lastGmedAt[msg.sender] = block.timestamp;
+
         totalGms += 1;
         console.log("%s has said gm!", msg.sender);
 
         // Store gm data in the array
         gms.push(Gm(msg.sender, _message, block.timestamp));
 
-        emit NewGm(msg.sender, block.timestamp, _message);
+        seed = (block.difficulty + block.timestamp + seed) % 100;
 
+        if (seed <= 50) {
         uint256 prizeAmount = 0.0000069 ether;
         require(
             prizeAmount <= address(this).balance,
@@ -38,6 +51,9 @@ contract GmPortal {
         );
         (bool success, ) = (msg.sender).call{value: prizeAmount}("");
         require(success, "Failed to withdraw money from contract");
+        }
+
+        emit NewGm(msg.sender, block.timestamp, _message);
     }
 
     // Return the gms struct array for the frontend
